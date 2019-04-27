c1558be6aa4b87de61698f58489b8d0b3f22908a
UltraGreed <35086784+UltraGreed@users.noreply.github.com>
2019-04-27 17:01:05 +1000

    Add files via upload

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 #self.rect = pygame.Rect(pos[0], pos[1], res[0], res[1])
 
@@ -10,24 +11,37 @@ player_res = (93, 60)
 enemy_res = (60, 93)
 bullet_res = (15, 48)
 
+back = 0
+new_level = True
+
+
+def create_enemy(line, n):
+    if line == 1:
+        y = 0
+    else:
+        y = 100
+    x = 65 * n
+    Enemy((x, y), random.choice(enemy_images), enemy_res)
+
 
 def shoot(pos, res):
-    bullets.append(bullet((pos[0] + res[0] // 2, pos[1] + res[1] // 2), 'bullet.png', 5))
+    bullets.append(bullet((pos[0] + res[0] // 2, pos[1] + res[1] // 2 - 90), 'bullet.png', 5))
 
 
 def move_all():
-    if Enemy.is_next_move_left:
-        if Enemy.enemies[0].get_x() - 70 >= 0:
-            for i in Enemy.enemies:
-                i.move(-10, 0)
+    global is_next_move_left
+    if is_next_move_left:
+        if Enemy.enemies[0].get_x() - 10 >= 0:
+            for enemy in Enemy.enemies:
+                enemy.move(-10, 0)
         else:
-            Enemy.is_next_move_left = False
+            is_next_move_left = False
     else:
-        if Enemy.enemies[-1].get_x() + 10 <= resolution[0]:
-            for i in Enemy.enemies:
-                i.move(10, 0)
+        if Enemy.enemies[-1].get_x() + 70 <= resolution[0]:
+            for enemy in Enemy.enemies:
+                enemy.move(10, 0)
         else:
-            Enemy.is_next_move_left = True
+            is_next_move_left = True
 
 
 class Thing:
@@ -92,10 +106,14 @@ resolution = 1600, 900
 window: pygame.Surface = pygame.display.set_mode(resolution, FULLSCREEN)
 fullscreen = True
 pygame.display.set_caption('Kremlin Travel')
+
 working = True
 bullets = list()
 player = Player([800, 700], 'player.png', player_res, 100)
-phone = Thing((0, 0), background_images[0], (1600, 900))
+background = Thing((0, 0), background_images[0], (1600, 900))
+enemy = Enemy((0, 0), 'enemy1.png', enemy_res)
+delay = 7
+x = delay
 while working:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -111,7 +129,9 @@ while working:
             if event.key == K_ESCAPE:
                 working = False
             if event.key == K_SPACE:
-                shoot(player.position, player.resolution)
+                if x == delay:
+                    shoot(player.position, player.resolution)
+                    x = 0
     keys = pygame.key.get_pressed()
     if keys[K_a]:
         if not player.get_x() - 10 <= 0:
@@ -125,11 +145,33 @@ while working:
     if keys[K_s]:
         if not player.get_y() + player.resolution[1] + 5 >= 900:
             player.move(0, 5)
-    phone.draw()
-    player.draw()
     for i in bullets:
         i.draw()
-        i.move(0, -10)
+        for enemy in Enemy.enemies.copy():
+            if collision(i.get_rect(), enemy.get_rect()):
+                Enemy.enemies.remove(enemy)
+        if collision(i.get_rect(), player.get_rect()):
+            player.dmg(1)
+        i.move(0, -25)
+    for i in Enemy.enemies:
+        i.draw()
+    if x < delay:
+        x += 1
+
+    if new_level:
+        new_level = False
+        for i in range(4):
+            create_enemy(1, i)
+    if not Enemy.enemies:
+        back += 1
+        new_level = True
+
+    if back % 3 == 0 and new_level:
+        background = Thing((0, 0), background_images[back % 3], (1600, 900))
+    player.draw()
+    if Enemy.enemies:
+        move_all()
+    pygame.time.Clock().tick(60)
     pygame.display.update()
-    pygame.time.Clock().tick(120)
+
 pygame.quit()
